f0319f4f45aa1e10fe281e207098a63b340e328e
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-20 13:07:40 +0100

    Refactored #check_environment

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,6 +2,18 @@ require 'pg'
 
 class Bookmark
 
+  def self.check_environment
+    if ENV['DB_ENV'] == 'test'
+      connection = which_db('bookmark_manager_test')
+    else
+      connection = which_db('bookmark_manager')
+    end
+  end
+
+  def self.which_db(database)
+    PG.connect(dbname: "#{database}")
+  end
+
   def self.all
     connection = check_environment
     results = connection.exec('SELECT * FROM bookmarks')
@@ -13,11 +25,4 @@ class Bookmark
     connection.exec("INSERT INTO bookmarks (url) VALUES ('#{url}')")
   end
 
-  def self.check_environment
-    if ENV['DB_ENV'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-  end
 end
